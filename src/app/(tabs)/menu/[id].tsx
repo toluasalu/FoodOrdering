77bdff216d6138ddd3af579603b47d9297b89a98
toluasalu <johnasalu13@gmail.com>
2024-03-29 03:48:42 +0100

    Completed UI for product details screen

--- src/app/(tabs)/menu/[id].tsx
+++ src/app/(tabs)/menu/[id].tsx
@@ -1,8 +1,9 @@
-import { View, Text, Image, StyleSheet } from 'react-native'
-import React from 'react'
+import { View, Text, Image, StyleSheet, Pressable } from 'react-native'
+import React, { useState } from 'react'
 import { Stack, useLocalSearchParams } from 'expo-router'
 import products from "@assets/data/products";
 import { defaultPizzaImage } from '@/components/ProductListIItem';
+import Button from '@/components/Button';
 
 const sizes = ['S', 'M', 'L', 'XL'];
 
@@ -10,7 +11,11 @@ const ProductDetails = () => {
 
     const { id } = useLocalSearchParams();
     const product = products.find((p) => p.id.toString() === id);
+    const [selectedSize, setSelectedSize] = useState('S');
 
+    const addToCart = () => {
+        console.warn('Adding to cart');
+    };
     if (!product) {
         return <Text>Product not found</Text>;
     }
@@ -20,9 +25,15 @@ const ProductDetails = () => {
             <Image source={{ uri: product.image || defaultPizzaImage }} style={styles.img} />
             <Text>Select Size</Text>
             <View style={{ marginVertical: 10, flexDirection: "row", justifyContent: "space-around" }}>
-                {sizes.map(size => <Text key={size.charAt(0)}>{size}</Text>)}
+                {sizes.map(size =>
+                    <Pressable onPress={() => setSelectedSize(size)} key={size.charAt(0)} style={[styles.size, { backgroundColor: selectedSize === size ? 'gainsboro' : 'white' }]}>
+                        <Text style={[styles.sizeTxt, { color: selectedSize === size ? 'black' : 'grey' }]}>{size}</Text>
+                    </Pressable>
+                )}
+
             </View>
             <Text style={styles.price}>{product.price}</Text>
+            <Button text='Add to cart' onPress={addToCart} />
         </View>
     )
 }
@@ -41,6 +52,18 @@ const styles = StyleSheet.create({
     price: {
         fontSize: 18,
         fontWeight: "bold"
+    },
+    size: {
+        backgroundColor: "gainsboro",
+        width: 50,
+        aspectRatio: 1,
+        borderRadius: 25,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    sizeTxt: {
+        fontSize: 20,
+        fontWeight: "500"
     }
 });
 
